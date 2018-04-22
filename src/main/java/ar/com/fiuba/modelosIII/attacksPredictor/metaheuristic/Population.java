1bb0d51e9854966b7550d405ecc8fe5ee1132c94
Gonzalo Vallerino <gonzalo.vallerino@mercadolibre.com>
2018-04-22 02:06:49 -0300

    Agrego test

--- src/main/java/ar/com/fiuba/modelosIII/attacksPredictor/metaheuristic/Population.java
+++ src/main/java/ar/com/fiuba/modelosIII/attacksPredictor/metaheuristic/Population.java
@@ -1,9 +1,7 @@
 package ar.com.fiuba.modelosIII.attacksPredictor.metaheuristic;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import ar.com.fiuba.modelosIII.attacksPredictor.model.TerroristAttack;
 import ar.com.fiuba.modelosIII.attacksPredictor.model.TerroristAttacksDataSet;
@@ -14,9 +12,12 @@ public class Population {
 	
 	public List<TerroristAttack> populate(List<TerroristAttack> filter) {
 		List<TerroristAttack> population = new ArrayList<TerroristAttack>();
-		List<TerroristAttack> dataSetFiltered = dataSet.filter(filter);
-		double proba = (double)dataSetFiltered.size() / (double)dataSet.getSize();
-		
+		List<TerroristAttack> dataSetFiltered = dataSet.getAll();
+		double proba = 0.5D;
+		if (filter != null) {
+			dataSetFiltered = dataSet.filter(filter);
+			proba = (double)dataSetFiltered.size() / (double)dataSet.getSize();
+		}	
 		for (TerroristAttack attack : dataSetFiltered) {
 			double random = Math.random() * 100.0;
 			boolean include = (proba > random);
--- src/test/java/ar/com/fiuba/modelosIII/attacksPredictor/files/EvolutionTest.java
+++ src/test/java/ar/com/fiuba/modelosIII/attacksPredictor/files/EvolutionTest.java
@@ -1,8 +1,11 @@
 package ar.com.fiuba.modelosIII.attacksPredictor.files;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import ar.com.fiuba.modelosIII.attacksPredictor.enums.BorneableEnum;
+import ar.com.fiuba.modelosIII.attacksPredictor.metaheuristic.Population;
 import ar.com.fiuba.modelosIII.attacksPredictor.metaheuristic.evolution.Borneable;
 import ar.com.fiuba.modelosIII.attacksPredictor.model.TerroristAttack;
 import ar.com.fiuba.modelosIII.attacksPredictor.model.TerroristAttacksDataSet;
@@ -11,6 +14,7 @@ public class EvolutionTest {
 	
 	private TerroristAttacksDataSet data = TerroristAttacksDataSet.getInstance();
 	private Borneable reproduction = BorneableEnum.REPRODUCTION.getBorn();
+	private Population population = new Population();
 	
 	@Test
 	public void sonTest() {
@@ -21,6 +25,13 @@ public class EvolutionTest {
 		father.print();
 		mother.print();
 		son.print();
+	}
+	
+	@Test
+	public void populationTest() {
+		List<TerroristAttack> populate = population.populate(null);
+		System.out.println(populate.size());
+		
 	}
 
 }
