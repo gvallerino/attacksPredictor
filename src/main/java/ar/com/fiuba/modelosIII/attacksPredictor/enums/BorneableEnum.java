c5c993d1da4caeca316d05f1b9cb9edcacfbb8d5
Gonzalo Vallerino <gonzalovallerino@mercadolibre.com>
2018-04-29 03:10:53 -0300

    Agrego reproduccion por propiedad al listado de reproducciones

--- src/main/java/ar/com/fiuba/modelosIII/attacksPredictor/enums/BorneableEnum.java
+++ src/main/java/ar/com/fiuba/modelosIII/attacksPredictor/enums/BorneableEnum.java
@@ -2,10 +2,12 @@ package ar.com.fiuba.modelosIII.attacksPredictor.enums;
 
 import ar.com.fiuba.modelosIII.attacksPredictor.metaheuristic.evolution.Borneable;
 import ar.com.fiuba.modelosIII.attacksPredictor.metaheuristic.evolution.ReproductionBySegmento;
+import ar.com.fiuba.modelosIII.attacksPredictor.metaheuristic.evolution.ReproductionByProperty;;
 
 public enum BorneableEnum {
 	
-	REPRODUCTION_SEGMENTO (1, new ReproductionBySegmento());
+	REPRODUCTION_SEGMENTO (1, new ReproductionBySegmento()),
+	REPRODUCTION_PROPERTY(2, new ReproductionByProperty());
 	
 	private int id;
 	private Borneable born;
