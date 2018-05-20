202df210d5be4282f532b5735f6e5abd85c82231
Gonzalo Vallerino <gonzalovallerino@mercadolibre.com>
2018-05-20 18:20:29 -0300

    Cambio muchas cosas

--- src/main/java/ar/com/fiuba/modelosIII/attacksPredictor/metaheuristic/GeneticAlgorithm.java
+++ src/main/java/ar/com/fiuba/modelosIII/attacksPredictor/metaheuristic/GeneticAlgorithm.java
@@ -31,72 +31,22 @@ public class GeneticAlgorithm {
 		
 		for (int generacion = 0; generacion < generaciones; generacion++) {
 			
-//			List<Double> prom1 = initListaPromedios();
-//			List<Double> prom2 = initListaPromedios();
 			List<Double> prom3 = initListaPromedios();
 			
 			for (int i = 0; i < repeticiones; i++) {
 				TerroristAttack father = poblacion.get(Constants.getRandom(0, poblacion.size()));
 				TerroristAttack mother = poblacion.get(Constants.getRandom(0, poblacion.size()));
-				TerroristAttack son = cruzaPorImportancia.cruzar(father, mother);
+				TerroristAttack son = cruzaBinaria.cruzar(father, mother);
 				proximaPoblacion.add(son);
 				
-//				prom1 = sumarListas(prom1, father.getValues());
-//				prom2 = sumarListas(prom2, mother.getValues());
 				prom3 = sumarListas(prom3, son.getValues());
 			}
 			
-//			printPromedio(prom1, repeticiones);
-//			printPromedio(prom2, repeticiones);
 			printPromedio(prom3, repeticiones);
 			System.out.println("Cambiando de generacion");
 			poblacion = proximaPoblacion;
 			proximaPoblacion = new ArrayList<TerroristAttack>();
 		}
-		
-		
-//		List<TerroristAttack> filter = createFilters();
-//		Population population = new Population();
-//		List<TerroristAttack> poblacion1 = population.populate(filter);
-//		List<TerroristAttack> poblacion2 = population.populate(filter);
-//		List<TerroristAttack> proximaPoblacion = new ArrayList<TerroristAttack>();
-//		
-//		printList(poblacion1);
-//		printList(poblacion2);
-//		
-//		List<Double> prom1 = initListaPromedios();
-//		List<Double> prom2 = initListaPromedios();
-//		List<Double> prom3 = initListaPromedios();
-//		
-//		int max = poblacion1.size() > poblacion2.size() ? poblacion1.size() : poblacion2.size();
-//		
-//		for (int i = 0; i < max; i++) {
-//			
-//			TerroristAttack father = null;
-//			TerroristAttack mother = null;
-//			TerroristAttack son = null;
-//			
-//			if (i< poblacion1.size()) {
-//				father = poblacion1.get(i);
-//				prom1 = sumarListas(prom1, father.getValues());
-//			}
-//			
-//			if (i < poblacion2.size()) {
-//				mother = poblacion2.get(i);
-//				prom2 = sumarListas(prom2, mother.getValues());
-//			}
-//				 
-//			if (father != null && mother != null) {
-//				son = cruzaPorSegmento.cruzar(father, mother);
-//				prom3 = sumarListas(prom3, son.getValues());
-//				proximaPoblacion.add(son);
-//			}
-//		}
-//		
-//		printList(proximaPoblacion);
-//		printPromedio(prom1, poblacion1.size());
-//		printPromedio(prom2, poblacion2.size());
-//		printPromedio(prom3, proximaPoblacion.size());
 		
 	}
 	
@@ -170,3 +120,47 @@ public class GeneticAlgorithm {
 	}
 	
 }
+
+
+//List<TerroristAttack> filter = createFilters();
+//Population population = new Population();
+//List<TerroristAttack> poblacion1 = population.populate(filter);
+//List<TerroristAttack> poblacion2 = population.populate(filter);
+//List<TerroristAttack> proximaPoblacion = new ArrayList<TerroristAttack>();
+//
+//printList(poblacion1);
+//printList(poblacion2);
+//
+//List<Double> prom1 = initListaPromedios();
+//List<Double> prom2 = initListaPromedios();
+//List<Double> prom3 = initListaPromedios();
+//
+//int max = poblacion1.size() > poblacion2.size() ? poblacion1.size() : poblacion2.size();
+//
+//for (int i = 0; i < max; i++) {
+//	
+//	TerroristAttack father = null;
+//	TerroristAttack mother = null;
+//	TerroristAttack son = null;
+//	
+//	if (i< poblacion1.size()) {
+//		father = poblacion1.get(i);
+//		prom1 = sumarListas(prom1, father.getValues());
+//	}
+//	
+//	if (i < poblacion2.size()) {
+//		mother = poblacion2.get(i);
+//		prom2 = sumarListas(prom2, mother.getValues());
+//	}
+//		 
+//	if (father != null && mother != null) {
+//		son = cruzaPorSegmento.cruzar(father, mother);
+//		prom3 = sumarListas(prom3, son.getValues());
+//		proximaPoblacion.add(son);
+//	}
+//}
+//
+//printList(proximaPoblacion);
+//printPromedio(prom1, poblacion1.size());
+//printPromedio(prom2, poblacion2.size());
+//printPromedio(prom3, proximaPoblacion.size());
--- src/main/java/ar/com/fiuba/modelosIII/attacksPredictor/model/TerroristAttack.java
+++ src/main/java/ar/com/fiuba/modelosIII/attacksPredictor/model/TerroristAttack.java
@@ -1,5 +1,6 @@
 package ar.com.fiuba.modelosIII.attacksPredictor.model;
 
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 
@@ -24,44 +25,53 @@ public class TerroristAttack {
 	private Integer amountKill;
 	private Integer amountWound;
 	
-	private List<Integer> fitnessValues;
 	private List<Integer> values;
-	private int fitness;
+	private BitSet valuesBinary;
 	
-	private BitSet valuesBinary = new BitSet(Constants.COUNT_DATA_TYPE_BINARY());
-	private int[] firstPositionsBinary = Constants.FIRST_POSITION_BINARY(); 
+	private List<Integer> fitnessValues;
+	private int fitness;
 	
 	//me quede en que tengo que elegir que, para cada atributo, que es lo mejor para el hijo. Por ejemplo, el año pondria un random. no me importa
 	//para los enums tengo que agregarle prioridad. para la cantidad de heridos y muertos elegiria el mayor (es mejor ataque).
 	//todas estas decisiones tengo que tomarlas
 	public TerroristAttack() {
-		
+		this.values = new ArrayList<Integer>();
+		this.valuesBinary = new BitSet(Constants.COUNT_DATA_TYPE_BINARY());
+		this.fitnessValues = new ArrayList<Integer>();
 	}
 	
 	public TerroristAttack (String id, BitSet bits) {
 		this.id = id;
 		this.valuesBinary = bits;
+		this.values = DecodeBinary.decode(bits);
+		this.buildTerroristAttackByValues();
+		this.processFitness();
 	}
 	
 	public TerroristAttack (String id, List<Integer> values) {
 		this.id = id;
+		this.values = values;
+		this.buildTerroristAttackByValues();
+		this.valuesBinary = CodeBinary.code(values);
+		this.processFitness();
+	}
+	
+	private void buildTerroristAttackByValues() {
 		for (int i = 0; i < values.size(); i++) {
 			int value = values.get(i).intValue();
 			switch (i) {
-				case 0: this.year = new Integer(value); this.yearToBinary(value); break;
-				case 1: this.region = RegionEnum.getById(value); this.typeToBinary(value, i); break;
-				case 2: this.multiple = new Boolean(value == 1); this.booleanToBinary(value, i); break;
-				case 3: this.success = new Boolean(value == 1); this.booleanToBinary(value, i); break;
-				case 4: this.suicide = new Boolean(value == 1); this.booleanToBinary(value, i); break;
-				case 5: this.attackType = AttackTypeEnum.getById(value); this.typeToBinary(value, i); break;
-				case 6: this.targetType = TargetTypeEnum.getById(value); this.typeToBinary(value, i); break;
-				case 7: this.weaponType = WeaponTypeEnum.getById(value); this.typeToBinary(value, i); break;
-				case 8: this.amountKill = new Integer(value); this.amountKillsToBinary(value); break;
-				case 9: this.amountWound = new Integer(value); this.amountWoundToBinary(value); break;
+				case 0: this.year = new Integer(value); break;
+				case 1: this.region = RegionEnum.getById(value); break;
+				case 2: this.multiple = new Boolean(value == 1); break;
+				case 3: this.success = new Boolean(value == 1); break;
+				case 4: this.suicide = new Boolean(value == 1); break;
+				case 5: this.attackType = AttackTypeEnum.getById(value); break;
+				case 6: this.targetType = TargetTypeEnum.getById(value); break;
+				case 7: this.weaponType = WeaponTypeEnum.getById(value); break;
+				case 8: this.amountKill = new Integer(value); break;
+				case 9: this.amountWound = new Integer(value); break;
 			}
 		}
-		this.values = values;
-		this.processFitness();
 	}
 	
 	private void processFitness() {
@@ -89,41 +99,7 @@ public class TerroristAttack {
 		System.out.println("]");
 	}
 	
-	private void yearToBinary(int year) {
-		int numerador = year > Constants.YEAR_MIN ? year - Constants.YEAR_MIN : 0;
-		int position = numerador / Constants.COUNT_DIVIDE_YEARS();
-		valuesBinary.flip(position);
-	}
 	
-	private void amountKillsToBinary(int amount) {
-		int numerador = (Constants.AMOUNT_KILLS_MAX > 0) ? Constants.AMOUNT_KILLS_MAX : 10000;
-		int divisor = numerador / Constants.COUNT_POSITION_BINARY[8];
-		int position = amount / divisor;
-		this.flip(position, 8);
-	}
-	
-	private void amountWoundToBinary(int amount) {
-		int numerador = (Constants.AMOUNT_WOUND_MAX > 0) ? Constants.AMOUNT_WOUND_MAX : 10000;
-		int divisor = numerador / Constants.COUNT_POSITION_BINARY[9];
-		int position = amount / divisor;
-		this.flip(position, 9);
-	}
-	
-	private void typeToBinary(int value, int caso) {
-		this.flip(value, caso);
-	}
-	
-	private void booleanToBinary(int value, int caso){
-		if (value == 1) {
-			flip(0, caso);
-		}
-	}
-	
-	private void flip(int value, int caso) {
-		int initial = firstPositionsBinary[caso];
-		int position = initial + value - 1;
-		valuesBinary.flip(position);
-	}
 	
 	public int getFitnessByProperty(int propertyPosition) {
 		return this.fitnessValues.get(propertyPosition);
@@ -143,7 +119,7 @@ public class TerroristAttack {
 
 	public void setYear(Integer year) {
 		this.year = year;
-		this.yearToBinary(year);
+		CodeBinary.codeYear(this.valuesBinary, year);
 	}
 
 	public RegionEnum getRegion() {
@@ -152,7 +128,7 @@ public class TerroristAttack {
 
 	public void setRegion(RegionEnum region) {
 		this.region = region;
-		this.typeToBinary(region.id, 1);
+		CodeBinary.codeByType(this.valuesBinary, region.id, 1);
 	}
 
 	public Boolean isMultiple() {
@@ -162,7 +138,7 @@ public class TerroristAttack {
 	public void setMultiple(Boolean multiple) {
 		this.multiple = multiple;
 		int value = multiple ? 1 : 0;
-		this.booleanToBinary(value, 2);
+		CodeBinary.codeBoolean(this.valuesBinary, value, 2);
 	}
 
 	public Boolean isSuccess() {
@@ -172,7 +148,7 @@ public class TerroristAttack {
 	public void setSuccess(Boolean success) {
 		this.success = success;
 		int value = success ? 1 : 0;
-		this.booleanToBinary(value, 3);
+		CodeBinary.codeBoolean(this.valuesBinary, value, 3);
 	}
 
 	public Boolean isSuicide() {
@@ -182,7 +158,7 @@ public class TerroristAttack {
 	public void setSuicide(Boolean suicide) {
 		this.suicide = suicide;
 		int value = suicide ? 1 : 0;
-		this.booleanToBinary(value, 4);
+		CodeBinary.codeBoolean(this.valuesBinary, value, 4);
 	}
 
 	public AttackTypeEnum getAttackType() {
@@ -191,7 +167,7 @@ public class TerroristAttack {
 
 	public void setAttackType(AttackTypeEnum attackType) {
 		this.attackType = attackType;
-		this.typeToBinary(attackType.id, 5);
+		CodeBinary.codeByType(this.valuesBinary, attackType.id, 5);
 	}
 
 	public TargetTypeEnum getTargetType() {
@@ -200,7 +176,7 @@ public class TerroristAttack {
 
 	public void setTargetType(TargetTypeEnum targetType) {
 		this.targetType = targetType;
-		this.typeToBinary(targetType.id, 6);
+		CodeBinary.codeByType(this.valuesBinary, targetType.id, 6);
 	}
 
 	public WeaponTypeEnum getWeaponType() {
@@ -209,7 +185,7 @@ public class TerroristAttack {
 
 	public void setWeaponType(WeaponTypeEnum weaponType) {
 		this.weaponType = weaponType;
-		this.typeToBinary(weaponType.id, 7);
+		CodeBinary.codeByType(this.valuesBinary, weaponType.id, 7);
 	}
 
 	public Integer getAmountKill() {
@@ -218,7 +194,7 @@ public class TerroristAttack {
 
 	public void setAmountKill(Integer amountKill) {
 		this.amountKill = amountKill;
-		this.amountKillsToBinary(amountKill);
+		CodeBinary.codeAmountKills(this.valuesBinary, amountKill);
 	}
 
 	public Integer getAmountWound() {
@@ -227,7 +203,7 @@ public class TerroristAttack {
 
 	public void setAmountWound(Integer amountWound) {
 		this.amountWound = amountWound;
-		this.amountWoundToBinary(amountWound);
+		CodeBinary.codeAmountWound(this.valuesBinary, amountWound);
 	}
 	
 	public int getFitness() {
